42af64eaa0cf08a8881da8c0b3c827f4b41c45e1
Luciano <roshinomo@gmail.com>
2026-04-27 22:54:36 -0300

    getting Started with Spring Boot

---
+++ src/main/java/com/score/garrys/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.score.garrys.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Backend Score QMod rodando no Railway!";
+    }
+}
