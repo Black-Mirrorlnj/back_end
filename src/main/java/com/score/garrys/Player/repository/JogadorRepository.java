c8ae1cb7db333dbb01d6e407573c4cd4d45b579c
Luciano <roshinomo@gmail.com>
2026-06-16 21:48:45 -0300

    remove query in jogador repository

--- src/main/java/com/score/garrys/Player/repository/JogadorRepository.java
+++ src/main/java/com/score/garrys/Player/repository/JogadorRepository.java
@@ -2,7 +2,6 @@ package com.score.garrys.Player.repository;
 
 import com.score.garrys.Player.model.Jogador;
 
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
@@ -18,11 +17,5 @@ public interface JogadorRepository extends JpaRepository<Jogador, Long> {
     //Buscar jogadores por nome (contendo parte do nome)
     List<Jogador> findByNomeContainingIgnoreCase(String nome);
 
-    //Buscar jogador com suas estatisticas ja carregadas
-    @Query ("SELECT j FROM Jogador j LEFT JOIN FETCH j.estatistica WHERE j.id = :id")
-    Optional<Jogador> findByIdWithEstatistica(Long id);
-
-    //Buscar jogador com ranking global ja carregado
-    @Query ("SELECT j FROM Jogador j LEFT JOIN FETCH j.rankingGlobal WHERE j.id = :id")
-    Optional<Jogador> findByIdWithRankingGlobal(Long id);
+    
 }
